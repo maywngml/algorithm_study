d777c9bd1db58ed09975bdde0f5263f7f7caa879
서주희 <maywngml@naver.com>
2024-08-23 00:29:21 +0900

    [Update] ✍️ 완주하지 못한 선수 재풀이

--- programmers/level1/완주하지 못한 선수.js
+++ programmers/level1/완주하지 못한 선수.js
@@ -5,26 +5,59 @@
 // 비교하면서 둘이 일치하면 completion에서 pop해벌임
 
 function solution(participant, completion) {
-    const completionHash = []
-    let answer = '';
-    
-    completion.forEach(name => {
-        if(name in completionHash) {
-            completionHash[name]++;
-        }
-        else {
-            completionHash[name] = 1;
-        }
-    })
-    
-    participant.forEach((name, index) => {
-        if(name in completionHash && completionHash[name] > 0) {
-            completionHash[name]--;
-        }
-        else {
-            answer = name;
-        }
-    })
-    
-    return answer;
+  const completionHash = [];
+  let answer = '';
+
+  completion.forEach((name) => {
+    if (name in completionHash) {
+      completionHash[name]++;
+    } else {
+      completionHash[name] = 1;
+    }
+  });
+
+  participant.forEach((name, index) => {
+    if (name in completionHash && completionHash[name] > 0) {
+      completionHash[name]--;
+    } else {
+      answer = name;
+    }
+  });
+
+  return answer;
+}
+
+// 처음 짠 코드, 효율성 통과 못 함 ^^; 쩝..
+function solution(participant, completion) {
+  const copiedParticipant = participant;
+  let answer;
+
+  completion.forEach((name) => {
+    const index = copiedParticipant.findIndex((item) => item === name);
+    copiedParticipant.splice(index, 1);
+  });
+
+  return copiedParticipant[0];
+}
+
+// 최종 정답
+function solution(participant, completion) {
+  const map = new Map();
+  let answer = '';
+
+  participant.forEach((name) => {
+    map.set(name, (map.get(name) || 0) + 1);
+  });
+  completion.forEach((name) => {
+    map.set(name, map.get(name) - 1);
+  });
+
+  map.forEach((value, key) => {
+    if (value > 0) {
+      answer = key;
+      return key;
+    }
+  });
+
+  return answer;
 }
