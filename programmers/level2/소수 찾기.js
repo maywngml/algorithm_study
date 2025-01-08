deafee308717e04e30ff7a334dfa15d62d213f58
서주희 <maywngml@naver.com>
2025-01-09 01:23:32 +0900

    [Update] ✍️ 소수 찾기

--- programmers/level2/소수 찾기.js
+++ programmers/level2/소수 찾기.js
@@ -51,3 +51,58 @@ function solution(numbers) {
 
   return answer;
 }
+
+function isPrime(number) {
+  const halfNumber = Math.floor(number / 2);
+  let result = true;
+
+  if (number === 0 || number === 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= halfNumber; i++) {
+    if (number % i === 0) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
+}
+
+// 다시 풀이
+// 시간복잡도: O(n * n!)
+function solution(numbers) {
+  const splitedNumbers = numbers.split('');
+  const splitedNumbersLength = splitedNumbers.length;
+  const combinatedNumbers = new Set();
+  let answer = 0;
+
+  function combination(length, str, checked) {
+    if (length === 0) {
+      combinatedNumbers.add(+str);
+      return;
+    }
+
+    for (let i = 0; i < splitedNumbersLength; i++) {
+      if (!checked[i]) {
+        checked[i] = true;
+        combination(length - 1, str + splitedNumbers[i], checked);
+        checked[i] = false;
+      }
+    }
+    return;
+  }
+
+  for (let i = 1; i <= splitedNumbersLength; i++) {
+    combination(i, '', Array(i).fill(false));
+  }
+
+  for (const number of combinatedNumbers.values()) {
+    if (isPrime(number)) {
+      answer++;
+    }
+  }
+
+  return answer;
+}
